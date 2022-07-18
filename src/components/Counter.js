939dacf47b67f04768e66a183c6a45509a7891e9
cesar.aguilar <Blueavian9@gmail.com>
2022-07-18 10:40:59 -0700

    initial

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,6 +6,7 @@ https://tk-assets.lambdaschool.com/59036a85-0980-42c8-81ad-9afc8354497f_counter-
 
 How many slices of state do you think are necessary to act as "sources of truth" for all
 the things that change in this widget? Give it some thought before continuing reading!
+
 A naive developer might say 3 different slices:
   - The count
   - Whether the text is color crimson or royalblue
@@ -40,50 +41,42 @@ STEP 4:
 STEP 5:
   This click handler needs to use 'setCount' to set the 'count' to be the current 'count' minus one.
   Do NOT do count--. That amounts to trying to mutate 'count' in place. This is the road to perdition.
+
 STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
- /* STEP 0 */
-import React, { useState } from 'react';
+
+import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Counter() {
   /* STEP 1 */
-const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
+
   const increment = () => {
-    setCount(count + 1)  
-  /* STEP 4 */
+    setCount(count + 1);
+    /* STEP 4 */
   };
   const decrement = () => {
-    setCount(count - 1)
+    setCount(count - 1);
     /* STEP 5 */
   };
   const reset = () => {
-    setCount(0)
+    setCount(0);
     /* STEP 6 */
   };
-
-// How to check if variable is even or odd??
-// Modulous => %
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: count % 2 === 0 ? 'royalblue' : 'crimson' /* STEP 2 */
+    color: (count % 2) === 0 ? 'royalblue' : "crimson"
+     /* STEP 2 */
   };
-
-/**
- * if {count % 2 === 0} {
- *   return 'royalBlue'
- * } else {
- *   return 'crimson'
- * }
- */
 
   return (
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number {count} is {count % 2 === 0 ? 'even' : 'odd'} {/* STEP 3 */}
+        Number {count} is {(count % 2) === 0 ? 'even' : 'odd'} {/* STEP 3 */}
       </div>
       <div>
         <button id='increment' onClick={increment}>Increment</button>
@@ -93,4 +86,3 @@ const [count, setCount] = useState(0);
     </div>
   );
 }
-
